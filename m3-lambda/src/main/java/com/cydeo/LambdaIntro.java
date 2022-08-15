9c7f01383f870149ea65312c95e511eab28e71a2
Ekaterina <ekaterina.sementeva@gmail.com>
2022-08-15 12:24:52 -0400

    Lambda Intro

---
+++ m3-lambda/src/main/java/com/cydeo/LambdaIntro.java
@@ -0,0 +1,14 @@
+package com.cydeo;
+
+public class LambdaIntro {
+    /*
+    Introduced in Java 8
+    Was good in OOP, but was missing Functional Programming
+    Lambda: enabled functional programming
+            more readable code structure
+
+    In Java everything is Object. You cannot have some individual actions alone.
+    If you want an action, you need to write a class then create object, then call it(function/method)
+
+     */
+}
