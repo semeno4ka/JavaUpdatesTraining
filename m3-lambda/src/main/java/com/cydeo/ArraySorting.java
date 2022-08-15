60f04921f6a7334177455fb9514de45126e79950
Ekaterina <ekaterina.sementeva@gmail.com>
2022-08-15 12:31:27 -0400

    OOP demo

--- m3-lambda/src/main/java/com/cydeo/ArraySorting.java
+++ m3-lambda/src/main/java/com/cydeo/ArraySorting.java
@@ -1,4 +1,14 @@
 package com.cydeo;
 
-public class ArraySorting {
+public class ArraySorting {// QuickSort,BubbleSOrt - ways to sort Array used before
+
+    public static void main(String[] args) {
+
+        ArraySorting as= new ArraySorting();
+        as.sort();
+
+    }
+    private void sort(){
+        System.out.println("Quick Sorting Array");
+    }
 }
