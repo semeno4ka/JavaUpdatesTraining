f89cf132c65362db352bcd315976c1203551c8a5
Ekaterina <ekaterina.sementeva@gmail.com>
2022-08-15 10:53:58 -0400

    swap Elements method

---
+++ m2-enum/src/main/java/com/cydeo/task/Hometask.java
@@ -0,0 +1,24 @@
+package com.cydeo.task;
+
+import java.util.Arrays;
+
+public class Hometask {
+
+    public static void main(String[] args) {
+        Integer[] arr={1,2,3,4,5};
+        String[] arr2 ={"milk","fire","chill","focus"};
+        System.out.println(Arrays.toString(swap(arr,0,3)));
+        System.out.println(Arrays.toString(swap(arr2,2,3)));
+    }
+
+    public static <T> T[] swap(T[] a, int index, int index2){
+        T[] temp= (T[]) new Object[2];
+
+        temp[0]=a[index];
+        temp[1]=a[index2];
+        a[index]=temp[1];
+        a[index2]=temp[0];
+
+        return a;
+    }
+}
