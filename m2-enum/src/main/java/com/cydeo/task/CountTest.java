63742073df0e176aa6cdd6795fd906a35fc0c02b
Ekaterina <ekaterina.sementeva@gmail.com>
2022-08-15 11:48:25 -0400

    Solution to Count Task

---
+++ m2-enum/src/main/java/com/cydeo/task/CountTest.java
@@ -0,0 +1,24 @@
+package com.cydeo.task;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CountTest {
+    public static void main(String[] args) {
+
+        List<Integer> ci = Arrays.asList(1, 2, 3, 4);
+        int count = countIf(ci, new OddPredicate());
+        System.out.println("Number of odd integers = " + count);
+    }
+
+    // why we need generic? - to execute Prime and Odd
+private static <T> int countIf(List<T> c,UnaryPredicate<T> p){// have to put Interface, because otherwise you either accept Odd or EVen objects
+      int count = 0;
+      for(T element:c){
+          if(p.test(element)){
+              ++count;
+          }
+      }
+      return count;
+}
+}
