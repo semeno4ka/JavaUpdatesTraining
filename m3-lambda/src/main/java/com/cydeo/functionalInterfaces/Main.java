0e38b20aa0da609e259e1b18ce8f54dc7b64fd96
Ekaterina <ekaterina.sementeva@gmail.com>
2022-08-16 15:48:53 -0400

    Lambda in work examples

---
+++ m3-lambda/src/main/java/com/cydeo/functionalInterfaces/Main.java
@@ -0,0 +1,35 @@
+package com.cydeo.functionalInterfaces;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        List<User> users=new ArrayList<>();
+        users.add(new User("Mike","Smith",35));
+        users.add(new User("Tom","Smoore",30));
+        users.add(new User("Liza","Brown",29));
+        users.add(new User("Misha","Gilmore",39));
+        // print all
+        users.forEach(user-> System.out.println(user));
+        //print all that starts with L
+        users.forEach(user-> System.out.println(user));
+        //or
+        print(users,user->true);// no condition, if it is user, than print
+
+        print(users,s-> s.getFirstName().startsWith("L"));
+    }
+
+    public static void print(List<User> users, Predicate<User> s){//you are going through user Objects
+        for(User user: users){
+            if(s.test(user)){// this returns true if it meets the condition you apply later with Lambda
+                System.out.println(user.toString());
+            }
+        }
+    }
+}
--- m3-lambda/src/main/java/com/cydeo/functionalInterfaces/User.java
+++ m3-lambda/src/main/java/com/cydeo/functionalInterfaces/User.java
@@ -6,8 +6,8 @@ import lombok.*;
 //@Getter
 //@AllArgsConstructor
 //@ToString
-@Data // puts everything except AllArgConstructor, it creates NoArgsConstructor
-@AllArgsConstructor
+@Data // puts everything except AllArgConstructor, it creates NoArgsConstructor, as well as all required getters/setters,tostring
+@AllArgsConstructor// if you manually add AllArg, you have then manually inout Noarg again, otherwise it gets deleted from Data(Only if you need both)
 public class User {
 
     private String firstName;
