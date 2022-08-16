6af7477a650c638c1ac9a85ceccc90806d438579
Ekaterina <ekaterina.sementeva@gmail.com>
2022-08-16 14:44:19 -0400

    comment of forEach() important

--- m3-lambda/src/main/java/com/cydeo/homeTask/apple/PrettyPrintApple.java
+++ m3-lambda/src/main/java/com/cydeo/homeTask/apple/PrettyPrintApple.java
@@ -12,23 +12,31 @@ public class PrettyPrintApple {
         inventory.add(new Apple(280, Color.GREEN));
         inventory.add(new Apple(380, Color.RED));
 
-        ApplePredicate appleLambda=apple-> "An apple of "+apple.getWeight()+"g.";
-        prettyPrintApple(inventory,appleLambda);
-        prettyPrintApple(inventory, apple->"A "+apple.getColor()+" apple");
+        ApplePredicate appleLambda = apple -> "An apple of " + apple.getWeight() + "g.";
+        prettyPrintApple(inventory, appleLambda);
+        prettyPrintApple(inventory, apple -> "A " + apple.getColor() + " apple");
+        // each apple gets print method unique implementation
 
-       // Example for more than one line
-       ApplePredicate fancyApple = apple->{
-          String ch = apple.getWeight()>200?"Heavy":"Light";
-          return "A "+ch+ " "+apple.getColor()+" apple";// if more than one line, curly braces required and return required as well
-      };
-prettyPrintApple(inventory,fancyApple);
-    }
+        inventory.forEach(apple -> System.out.println(apple));// to print each without using the loop
 
-    public static void prettyPrintApple(List<Apple> inventory, ApplePredicate applePredicate){
-    for(Apple apple: inventory){
-        String output= applePredicate.print(apple);
-        System.out.println(output);
-    }
+        // Example for more than one line
+        ApplePredicate fancyApple = apple -> { // you can specify implementation within Lambda
+            String ch = apple.getWeight() > 200 ? "Heavy" : "Light";
+            return "A " + ch + " " + apple.getColor() + " apple";// if more than one line, curly braces required and return required as well
+        };
+        prettyPrintApple(inventory, fancyApple);
+
+
 
     }
+
+    public static void prettyPrintApple(List<Apple> inventory, ApplePredicate applePredicate) {
+        for (Apple apple : inventory) { // the interface is responsible for ability to execute unique action
+            String output = applePredicate.print(apple);
+            System.out.println(output);
+            //we check every apple and apply print method to each of the apple. However, specific implementation will be passed through Lambda
+        }
+    }
+
+
 }
