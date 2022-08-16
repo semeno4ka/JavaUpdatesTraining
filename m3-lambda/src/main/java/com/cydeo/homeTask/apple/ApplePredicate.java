f836293193af61693b81636518794f7f18405960
Ekaterina <ekaterina.sementeva@gmail.com>
2022-08-16 15:05:30 -0400

    comment of forEach() important

--- m3-lambda/src/main/java/com/cydeo/homeTask/apple/ApplePredicate.java
+++ m3-lambda/src/main/java/com/cydeo/homeTask/apple/ApplePredicate.java
@@ -1,5 +1,7 @@
 package com.cydeo.homeTask.apple;
 
 public interface ApplePredicate {
-     String print(Apple apple);// because we will use the particular feature of particular object(color, weight)
+     String print(Apple apple);
+
+     // because we will print the particular feature of particular object(color, weight)
 }
