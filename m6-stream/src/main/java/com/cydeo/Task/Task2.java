378b50055b935d91eef2bd94ebab47b471c0a0e1
Ekaterina <ekaterina.sementeva@gmail.com>
2022-08-20 17:15:49 -0400

    adjustments

--- m6-stream/src/main/java/com/cydeo/Task/Task2.java
+++ m6-stream/src/main/java/com/cydeo/Task/Task2.java
@@ -10,23 +10,44 @@ public class Task2 {
         System.out.println("LESS THAN 400: name, calorie");
         DishData.getAll().stream()
         .filter(dish->dish.getCalories()<400)
-                .map(dish -> dish.getName()+":"+dish.getCalories())
+                .map(dish -> dish.getName()+":"+dish.getCalories())// if needed just a name, could go with::
+                // .map(Dish::getName)
         .forEach(System.out::println);
         System.out.println("EACH DISH NAME LENGTH");
         DishData.getAll().stream()
                 .map(dish->dish.getName()+":"+dish.getName().length())
                 .forEach(System.out::println);
+        /*
+        Could be:
+        .map(Dish::getName)
+        .map(String::getLength)
+        .forEach(System.out::println)
+         */
         System.out.println("HIGH CALORIE >300: name, calorie");
         DishData.getAll().stream()
                 .filter(dish->dish.getCalories()>300)
                 .map(dish->dish.getName()+":"+dish.getCalories())
+                .limit(3)
                 .forEach(System.out::println);
+        /*
+         .filter(dish->dish.getCalories()>300)
+         .map(Dish::getName)
+         .limit(3)
+         .forEach(System.out::println)
+
+         */
         System.out.println("BELOW 400 SORTED:name,calorie");
         DishData.getAll().stream()
                 .filter(dish->dish.getCalories()<400)
                 .sorted(comparing(Dish::getCalories))// comparing method must be imported
                 .map(dish -> dish.getName()+":"+ dish.getCalories())
                 .forEach(System.out::println);
+        /*
+          .filter(dish->dish.getCalories()<400)
+          .sorted(comparing(Dish::getCalories))
+          .map(Dish::getName)
+          .forEach(System.out::println);
+         */
 
 
 
