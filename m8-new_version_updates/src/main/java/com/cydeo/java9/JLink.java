8d73a22e52d13bae4279a54e8b86d3f3f113993e
Ekaterina <ekaterina.sementeva@gmail.com>
2022-08-22 19:48:50 -0400

    JLINK update

---
+++ m8-new_version_updates/src/main/java/com/cydeo/java9/JLink.java
@@ -0,0 +1,17 @@
+package com.cydeo.java9;
+
+public class JLink {
+    /*
+    Java command line tool that is used to generate A CUSTOM JRE (Java runtime environment)!!!
+
+    With JLINK we can create our own, small JRE that contains only the relevant classes that we want to use, without wasting memory,
+    increasing performance
+
+    Before:
+    To run a simple class, you will have to run all 4300 classes, for 4kb class, you will use 204mg of memory.
+    After:
+    creates custom JRE which run only the classes and library you need
+
+
+     */
+}
