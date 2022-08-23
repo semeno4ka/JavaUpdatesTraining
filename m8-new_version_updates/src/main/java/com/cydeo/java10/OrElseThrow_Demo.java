e4087a3d68d14eb2c98d4896df662fcafd4bccdd
Ekaterina <ekaterina.sementeva@gmail.com>
2022-08-23 12:46:54 -0400

    orElseThrow

---
+++ m8-new_version_updates/src/main/java/com/cydeo/java10/OrElseThrow_Demo.java
@@ -0,0 +1,35 @@
+package com.cydeo.java10;
+
+import java.util.Optional;
+
+public class OrElseThrow_Demo {
+    /*
+    combines get and isPresent in one code. to avoid NullPointerException
+    Will probably replace get
+
+    get doen't check if the element exist, it throws exception right away
+    orElseThrow checks if element is Present, if yes, returns value, if not, throws exception
+
+    Imagine using .get() method in your application in a lot of places, so if you get NoSuchElementException you
+     won't be able to know from which .get() method it is coming, but with orElseThrow() you will be able to throw
+     any exception you want in anywhere in your application, so that you can know exactly where 3exception is coming from.
+     */
+    public static void main(String[] args) {
+
+        Optional<String> str = Optional.empty();
+       // System.out.println(str.get());NoSuchElementException: No value present
+
+        //Behind the scene
+        if(str.isPresent()){
+        System.out.println(str.get());
+        }else{
+         //exception handeling
+        }
+
+        System.out.println(str.orElseThrow());
+        System.out.println(str.orElseThrow(()-> new RuntimeException()));
+
+
+
+    }
+}
