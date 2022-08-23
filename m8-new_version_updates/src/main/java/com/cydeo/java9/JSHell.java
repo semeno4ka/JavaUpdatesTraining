f27d420a006ace6a3d43c946bc3cbc6db09c02da
Ekaterina <ekaterina.sementeva@gmail.com>
2022-08-22 20:05:11 -0400

    JSHell

---
+++ m8-new_version_updates/src/main/java/com/cydeo/java9/JSHell.java
@@ -0,0 +1,19 @@
+package com.cydeo.java9;
+
+public class JSHell {
+    /*
+    You can go to terminal and write jshell in order to write and execute code directly without the need to write down classes, main method in Intellij
+
+    EXAMPLE :
+    Last login: Mon Aug 22 10:29:49 on console
+    ekaterinasementeva@Ekaterinas-MBP ~ % jshell
+    Welcome to JShell -- Version 11.0.14
+    For an introduction type: /help intro
+    jshell> System.out.println("Cydeo")
+    Cydeo
+    jshell>
+
+    You can do anything related to Java
+
+     */
+}
